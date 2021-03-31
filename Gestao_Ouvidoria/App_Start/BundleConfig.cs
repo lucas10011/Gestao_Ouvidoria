dd0a610403ae05451cd61007aa541cf7cb854bc3
Lucas Martins Guimarães <lucas_10011@hotmail.com>
2021-03-31 16:13:44 -0300

    Correção de alguns detalhes no sistema

--- Gestao_Ouvidoria/App_Start/BundleConfig.cs
+++ Gestao_Ouvidoria/App_Start/BundleConfig.cs
@@ -14,6 +14,9 @@ namespace Gestao_Ouvidoria
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/datatablejs").Include(
+                        "~/Scripts/datatable.js*"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -25,6 +28,9 @@ namespace Gestao_Ouvidoria
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/datatablecss").Include(
+                     "~/Content/datatable.css"));
         }
     }
 }
